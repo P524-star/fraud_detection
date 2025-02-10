60524e6872f763969f7261304d97c3d600b3c5cc
P524-star <traveltrippics17@gmail.com>
2025-02-11 01:45:21 +0530

    added 3 new ml model code

--- mlmodel.py
+++ mlmodel.py
@@ -1,3 +1,12 @@
 
 
 print("addig ml model code ")
+
+def model1():
+    print("adding code for ML Model1")
+
+def model1():
+    print("adding code for ML Model2")
+
+def model1():
+    print("adding code for ML Model3")   
